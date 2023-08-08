7004e8a127ab2364d82e4d4d6f913cf46d6d0b00
phankons <stephen.han@gmx.com>
2023-08-08 07:27:41 -0400

    Create Navigation and Profile Components from HTML

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react'; //optional
+import profileImage from '../profile-hex.png';
 
-  function Profile(){
-    return (
-      <h1>I am the profile component</h1>
-    );
-  }
+function Profile() {
+  return (
+    <aside>
+      <div class="profile">
+        <img src={profileImage} class="profile__image"/>;
+      </div>
+      <br />
+        <div class="profile__name">
+          <h2><span class="profile--bold">Amy</span> Mansell</h2>
+        </div>
+    </aside>
+  );
+}
 
-  export default Profile;
+export default Profile;
