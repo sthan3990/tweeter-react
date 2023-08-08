922e554e3f084acd5e0f970c1b3bb0188ad9ebb4
phankons <stephen.han@gmx.com>
2023-08-08 07:52:24 -0400

    Remove Hard Coded Value for avatar and name in Profile.js

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,15 +1,19 @@
 import React from 'react'; //optional
-import profileImage from '../assets/profile-hex.png';
 
 function Profile() {
+
+  const firstName = "Amy";
+  const lastName = "Mansell";
+  const avatar = "/profile-hex.png";
+
   return (
     <aside>
       <div className="profile">
-        <img src={profileImage} className="profile__image" alt="profile_image"/>;
+        <img src={avatar} className="profile__image" alt="profile_image"/>
       </div>
       <br />
         <div className="profile__name">
-          <h2><span className="profile--bold">Amy</span> Mansell</h2>
+          <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
         </div>
     </aside>
   );
